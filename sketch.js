3d72ede1f61929ccda3b8987840cf0374d52a7b8
ikenna <ikenna.porter@gmail.com>
2022-03-31 22:19:24 +0200

    Add cells to grid and change color when hovered over

--- sketch.js
+++ sketch.js
@@ -1,20 +1,37 @@
 const pad = document.querySelector(".sketch-pad");
 const pixels = document.querySelector("#pixel-range");
-const firstDiv = document.querySelector("#first-div");
+const cell = document.querySelector("#cell");
 
 
-//Fills grid up with children that form the background of the sketch pad
+createGrid()
+pixels.addEventListener('drag', e => createGrid());
+
+//Fills grid up with cells
 function createGrid() {
-    //Creates spans that will later be used to represent each square in the grid     
-    const div = document.createElement('div');
-    div.style.backgroundColor = 'white';
-    div.style.width = 'auto';
 
-    //populates the grid with the span elements
-    for (let i = 0; i < Number(pixels.value); i++) {
-        console.log(i);
-        firstDiv.parentNode.insertBefore(div, firstDiv.nextSibling);
+    //created appropriate number of rows and columns according to user's input
+    pad.style.gridTemplateColumns = `repeat(${pixels.value}, 1fr)`;
+    pad.style.gridTemplateRows = `repeat(${pixels.value}, 1fr)`;
+
+    for (let i = 0; i < pixels.valueAsNumber; i++) {
+        for (let j = 0; j < pixels.valueAsNumber; j++) {
+        //creates div which will later be added to pad as cells
+        const div = document.createElement('div');
+        //appends div to sketch pad one by one 
+        pad.appendChild(div).classList.add("cell");
+        }
     }
 }
 
-createGrid();
+//when hovered over div in pad, it turns black
+function changeCellColors() {
+    pad.addEventListener('mousemove', e => e.target.style.backgroundColor = 'black');
+}
+
+//erases cells in sketch pad
+function eraseCellColors() {
+    pad.addEventListener('mouseover', e => e.target.style.backgroundColor = 'white');
+}
+
+
+changeCellColors();
