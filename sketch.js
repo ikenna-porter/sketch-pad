3abf0044003332449f71a1175e733ff329991fdd
ikenna <ikenna.porter@gmail.com>
2022-04-01 21:04:02 +0200

    Remove cell count reset bug

--- sketch.js
+++ sketch.js
@@ -2,19 +2,29 @@ const pad = document.querySelector(".sketch-pad");
 const pixels = document.querySelector("#pixel-range");
 const cell = document.querySelector("#cell");
 
+createGrid(10);
 
-createGrid()
-pixels.addEventListener('drag', e => createGrid());
+pixels.addEventListener('change', function (e) {
+    cleanGrid();
+    createGrid(e.target.valueAsNumber);
+});
+
+//Removes all divs from grid, which allows cell count variability 
+function cleanGrid () {
+    while (pad.firstChild) {
+        pad.removeChild(pad.firstChild);
+    }
+}
 
 //Fills grid up with cells
-function createGrid() {
+function createGrid(e) {
 
     //created appropriate number of rows and columns according to user's input
-    pad.style.gridTemplateColumns = `repeat(${pixels.value}, 1fr)`;
-    pad.style.gridTemplateRows = `repeat(${pixels.value}, 1fr)`;
+    pad.style.gridTemplateColumns = `repeat(${e}, 1fr)`;
+    pad.style.gridTemplateRows = `repeat(${e}, 1fr)`;
 
-    for (let i = 0; i < pixels.valueAsNumber; i++) {
-        for (let j = 0; j < pixels.valueAsNumber; j++) {
+    for (let i = 0; i < e; i++) {
+        for (let j = 0; j < e; j++) {
         //creates div which will later be added to pad as cells
         const div = document.createElement('div');
         //appends div to sketch pad one by one 
@@ -32,6 +42,3 @@ function changeCellColors() {
 function eraseCellColors() {
     pad.addEventListener('mouseover', e => e.target.style.backgroundColor = 'white');
 }
-
-
-changeCellColors();
